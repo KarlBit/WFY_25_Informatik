094fdc9d62205273c806c74a8df54de9af88a989
Angonoka <33026971+KarlBit@users.noreply.github.com>
2025-02-21 11:10:21 +0100

    asd

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+# Import von Bibliotheken
+import math # (Beispielimport)
+
+# Hauptfunktion
+def main():
+    try:
+        print("Taschenrechner wird gestartet!")
+    except Exception as e:
+        print(f"Fehler: {e}")
+
+if __name__ == "__main__":
+    main()
