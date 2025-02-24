782a9f0d4f6f23cc97cfceedafa78176befe6ad6
Angonoka <33026971+KarlBit@users.noreply.github.com>
2025-02-24 14:29:20 +0100

    Änderungen main

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import math # (Beispielimport)
+import tkinter as tk
+
+# TKinter Fensterdefinition
+
+class Tetris_GUI:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Tetris Testprogramm")
+        self.window.geometry("400x400")
+        self.window.resizable(True, True)
+        
+        self.label = tk.Label(text="Tetris Beispiel")
+        self.label.pack()
+ 
+        self.window.mainloop()
 
 # Hauptfunktion
 def main():
     try:
-        print("Taschenrechner wird gestartet!")
+        print("Terminal Logging: Starte GUI")
+        gui = Tetris_GUI()
+
     except Exception as e:
         print(f"Fehler: {e}")
 
