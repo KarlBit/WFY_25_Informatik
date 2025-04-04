3f11c5948d04a40360370bd0b9d5ff7b125d93b6
Angonoka <33026971+KarlBit@users.noreply.github.com>
2025-04-04 09:58:19 +0200

    :pencil: Konifgurationsdatei und Logging Testing

--- 03_Website/webserver.py
+++ 03_Website/webserver.py
@@ -1,11 +1,19 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+# Import der Konfigurationsdatei (Installation mit "pip install pyyaml")
+import yaml
+
+# Einlesen der Konfigurationsdatei
+with open("configuration.yaml", "r", encoding="utf-8") as f:
+    config = yaml.safe_load(f)
+
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
+        self.wfile.write(b'configuration[HELLO_WORLD]')
 
 
 httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
--- 04_Logging_Konfigurationsdatei/main.py
+++ 04_Logging_Konfigurationsdatei/main.py
@@ -1,31 +1,65 @@
+''' Testprogramm für die Verwendung einer Konfigurationsdatei mit Logging'''
+# Imports
+# Standardbibliotheken
+import os
 import logging
 
-# Importieren der YAML-Bibliothek ("pip install pyyaml")
+# Dritte Bibliotheken
+# Zum finder der Bibliotheken nutze zum Beispiel "pypi.org"
+# YAML MODUL ("install pyyaml")
 import yaml
 
-# Laden der YAML-Konfigurationsdatei#
+# Config Datei Überprüfung
+if not os.path.exists("config.yaml"):
+    print("Konfigurationsdatei nicht gefunden!")
+    exit(1)
+else:
+    print("Konfigurationsdatei gefunden!")
+
+# Einlesen der Konfigurationsdatei
 with open("config.yaml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
-# Konfiguration des Loggers
-#logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
-logging.basicConfig(level=config["LOGGING_LEVEL"], format=config["LOGGING_FORMAT"])
+# Überprüfen, ob die Logging Datei existiert
+if not os.path.exists(config["LOGGING_PATH"]):
+    print("Log Datei nicht gefunden!")
+    exit(1)
+else:
+    print("Log Datei gefunden!")
 
-# Variablen
-a = 1
-b = 2
+# Logging Konfiguration
+logging.basicConfig(
+    level=config["LOGGING_LEVEL"],
+    format=config["LOGGING_FORMAT"],
+    handlers=[
+        # Datei Logging
+        logging.FileHandler(config["LOGGING_PATH"], encoding="utf-8"),
+        # Konsolen Logging
+        logging.StreamHandler()
+    ]
+)
 
+# Setzen der Variablen
+VARIABLE_A = float(config["VARIABLE_1"])
+VARIABLE_B = float(config["VARIABLE_2"])
+
+''' Hautpfunktion für das Modultesting'''
 def main():
+    logging.info("Starte das Programm...")
     # Versuche auszuführen
     try:
-        logging.info(f"Starte Berechnung: {a} + {b}")
-        logging.info(f"Ergebnis: {a} + {b} = {(a + b)}")
+        logging.debug("Starte die try Anweisung...")
+        logging.warning("Das ist eine warning...")
+        logging.error("Berechnung erfolgreich")
 
-    # Fehlerbehandlung (Falls ein Fehler auftritt)
+        logging.critical("Ergebnis: %d + %d = %d", VARIABLE_A, VARIABLE_B, VARIABLE_A + VARIABLE_B)
+        logging.info("Das Programm läuft erfolgreich.")
+
+    # Exception und Fehlerbehandlung
     except Exception as e:
-        # Logge den Fehler
-        logging.error(f"Fehler: {e}")
-
+               # Logge den Fehler
+        logging.error("Fehler: %s", e)
+        logging.debug("Irgendwas ist schiefgelaufen")
 
 # Starte die Hauptfunktion
 if __name__ == "__main__":
